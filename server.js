9897cc7373910647660bae2bb0a6899a150118ec
GopalSaraf <sarafgopal37@gmail.com>
2025-01-12 00:21:47 +0530

    Updated broker connection handling

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const cors = require("cors");
 const error = require("./middlewares/error");
 const routes = require("./routes/routes");
 const { DB } = require("./database");
+const Broker = require("./services/broker/broker");
 
 module.exports = async (app) => {
   await DB.connect();
+  await Broker.connect();
 
   app.use(express.json());
   app.use(cors());
