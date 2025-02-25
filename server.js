4436b2e221786fa28b4400f43d1444f1e7a0aa74
unknown <prathameshshriramwar100@gmail.com>
2025-02-25 11:11:49 +0530

    fixing production issue

--- server.js
+++ server.js
@@ -4,20 +4,26 @@ const cors = require("cors");
 const error = require("./middlewares/error");
 const routes = require("./routes/routes");
 const { DB } = require("./database");
+var bodyParser = require("body-parser");
 const Broker = require("./services/broker/broker");
 const { TalentPoolService } = require("./services/rpcandeventservice");
 const RPCService = require("./services/broker/rpc");
 const EventService = require("./services/broker/events");
 const { SERVICE_QUEUE } = require("./config/index");
+const multer = require("multer");
+const upload = multer();
 
 module.exports = async (app) => {
   await DB.connect();
   // await Broker.connect();
 
   app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(bodyParser.urlencoded({ extended: false }));
   app.use(cors());
   app.use(routes);
   app.use(error);
+  app.use(upload.array());
 
 
 
